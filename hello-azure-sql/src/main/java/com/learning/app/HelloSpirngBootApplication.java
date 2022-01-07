b9c53e87a6e7e579449e7447070e84808e785ab5
Akshay Tiwari <axedestined2succeed@gmail.com>
2022-01-07 06:44:05 +0000

    Using JDBC Template

--- hello-azure-sql/src/main/java/com/learning/app/HelloSpirngBootApplication.java
+++ hello-azure-sql/src/main/java/com/learning/app/HelloSpirngBootApplication.java
@@ -2,9 +2,10 @@ package com.learning.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 @ComponentScan("com.learning")
 public class HelloSpirngBootApplication {
 
--- hello-azure-sql/src/main/java/com/learning/controller/EmployeeController.java
+++ hello-azure-sql/src/main/java/com/learning/controller/EmployeeController.java
@@ -1,12 +1,9 @@
 package com.learning.controller;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import javax.sql.DataSource;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,49 +11,41 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EmployeeController {
 
-	// TODO: put in your connection string here. Also put in correct password
-	String connectionString = "jdbc:sqlserver://tiwarakssqlserver.database.windows.net:1433;database=tiwarakssqldb;user=akshay@tiwarakssqlserver;password=Password@123;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
-
 	@RequestMapping("get/{empName}")
-	public String getSecret(@PathVariable("empName") String empName) {
+	public String getEmployee(@PathVariable("empName") String empName) {
 		System.out.println(String.format("Geting Employee %s", empName));
-		String empNameDB = "";
+
 		int empSalDB = 0;
 
 		try {
-			Connection conn = DriverManager.getConnection(connectionString);
-			Statement statement = conn.createStatement();
-			ResultSet resultSet = statement
-					.executeQuery(String.format("SELECT EMP_NAME,EMP_SAL FROM EMPLOYEE where EMP_NAME='%s'", empName));
-			resultSet.next();
-			empNameDB = resultSet.getString("EMP_NAME");
-			empSalDB = resultSet.getInt("EMP_SAL");
+			empSalDB = new JdbcTemplate(mysqlDataSource()).queryForObject(
+					String.format("SELECT EMP_SAL FROM EMPLOYEE where EMP_NAME='%s'", empName), Integer.class);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return e.getMessage();
 		}
 
-		return String.format("Employee Name is %s and Salary is %s", empNameDB, empSalDB);
+		return String.format("Employee Name is %s and Salary is %s", empName, empSalDB);
 	}
 
 	@RequestMapping("create/{empName}/{empSal}")
 	public String insertEmployee(@PathVariable("empName") String empName, @PathVariable("empSal") int empSal) {
 		System.out.println(String.format("Inserting Employee %s:%s", empName, empSal));
-		Connection conn;
-		try {
-			conn = DriverManager.getConnection(connectionString);
-			PreparedStatement insertStatement = conn
-					.prepareStatement("INSERT INTO EMPLOYEE (EMP_NAME,EMP_SAL) VALUES (?, ?);");
-
-			insertStatement.setString(1, empName);
-			insertStatement.setInt(2, empSal);
-			insertStatement.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return e.getMessage();
-		}
-
+		new JdbcTemplate(mysqlDataSource()).update("INSERT INTO EMPLOYEE (EMP_NAME,EMP_SAL) VALUES (?, ?)",
+				new Object[] { empName, empSal });
 		return "Inserted Employee " + empName;
+	}
+
+	private DataSource mysqlDataSource() {
+		// TODO: put in your connection string here. Also put in correct password and
+		// username
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		// dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("");
+		dataSource.setUsername("");
+		dataSource.setPassword("");
+
+		return dataSource;
 	}
 
 }
